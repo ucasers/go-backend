58440b655d77d128217fb76861610e514a905161
MainJay <1913090@mail.nankai.edu.cn>
2024-05-28 14:23:12 +0800

    change gitignore

--- backend/controllers/users_controller.go
+++ backend/controllers/users_controller.go
@@ -2,40 +2,37 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/ucasers/go-backend/backend/models"
-	"net/http"
-	"strconv"
 )
 
 func (server *Server) GetUser(c *gin.Context) {
 
 	//clear previous error if any
-	errList := map[string]string{}
-
-	userID := c.Param("id")
-
-	uid, err := strconv.ParseUint(userID, 10, 32)
-	if err != nil {
-		errList["Invalid_request"] = "Invalid Request"
-		c.JSON(http.StatusBadRequest, gin.H{
-			"status": http.StatusBadRequest,
-			"error":  errList,
-		})
-		return
-	}
-	user := models.User{}
-
-	userGotten, err := user.FindUserByID(server.DB, uint32(uid))
-	if err != nil {
-		errList["No_user"] = "No User Found"
-		c.JSON(http.StatusNotFound, gin.H{
-			"status": http.StatusNotFound,
-			"error":  errList,
-		})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"status":   http.StatusOK,
-		"response": userGotten,
-	})
+	//errList := map[string]string{}
+	//
+	//userID := c.Param("id")
+	//
+	//uid, err := strconv.ParseUint(userID, 10, 32)
+	//if err != nil {
+	//	errList["Invalid_request"] = "Invalid Request"
+	//	c.JSON(http.StatusBadRequest, gin.H{
+	//		"status": http.StatusBadRequest,
+	//		"error":  errList,
+	//	})
+	//	return
+	//}
+	//user := models.User{}
+	//
+	//userGotten, err := user.FindUserByID(server.DB, uint32(uid))
+	//if err != nil {
+	//	errList["No_user"] = "No User Found"
+	//	c.JSON(http.StatusNotFound, gin.H{
+	//		"status": http.StatusNotFound,
+	//		"error":  errList,
+	//	})
+	//	return
+	//}
+	//c.JSON(http.StatusOK, gin.H{
+	//	"status":   http.StatusOK,
+	//	"response": userGotten,
+	//})
 }
